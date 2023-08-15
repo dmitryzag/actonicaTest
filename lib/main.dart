7d8576ea337d5f5cf036a75d5227d28a8672f44a
DAZ <zagibalovdmitry@gmail.com>
2023-08-15 16:59:44 +0700

    комм

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,56 @@
-import 'package:actonic_adboard/screens/advert_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:actonic_adboard/screens/advert_maker.dart';
+// import 'package:actonic_adboard/screens/advert_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:actonic_adboard/screens/advert_maker.dart';
 
-import 'models/database.dart';
+// import 'models/database.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SQLHelper.db();
-  runApp(const MyApp());
-}
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await SQLHelper.db();
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
 
-class _MyAppState extends State<MyApp> {
-  int currentIndex = 0;
-  final List<Widget> screens = const [
-    AdvertScreen(),
-    AdvertScreen(isFavorite: true),
-    AdvertMaker(null)
-  ];
+// class _MyAppState extends State<MyApp> {
+//   int currentIndex = 0;
+//   final List<Widget> screens = const [
+//     AdvertScreen(),
+//     AdvertScreen(isFavorite: true),
+//     AdvertMaker(null)
+//   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: screens[currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          currentIndex: currentIndex,
-          onTap: (index) => setState(() => currentIndex = index),
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Объявления',
-                backgroundColor: Colors.blue),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: 'Избранное',
-                backgroundColor: Colors.blue),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.create),
-                label: 'Создать объявление',
-                backgroundColor: Colors.blue)
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: screens[currentIndex],
+//         bottomNavigationBar: BottomNavigationBar(
+//           type: BottomNavigationBarType.fixed,
+//           currentIndex: currentIndex,
+//           onTap: (index) => setState(() => currentIndex = index),
+//           items: const [
+//             BottomNavigationBarItem(
+//                 icon: Icon(Icons.home),
+//                 label: 'Объявления',
+//                 backgroundColor: Colors.blue),
+//             BottomNavigationBarItem(
+//                 icon: Icon(Icons.favorite),
+//                 label: 'Избранное',
+//                 backgroundColor: Colors.blue),
+//             BottomNavigationBarItem(
+//                 icon: Icon(Icons.create),
+//                 label: 'Создать объявление',
+//                 backgroundColor: Colors.blue)
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
